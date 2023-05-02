dfc3173d428c1caa24831238c31dc0aa3fc60fda
Narawit Sukwattanajaroon <narawichsukwattanajaroon@gmail.com>
2023-05-03 00:00:11 +0700

    fix: init schema

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE "users" (
   "id" serial PRIMARY KEY,
   "email" varchar NOT NULL,
   "encrypted_password" varchar NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT 'now ()',
-  "updated_at" timestamp NOT NULL DEFAULT 'now ()'
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "todos" (
@@ -11,9 +11,9 @@ CREATE TABLE "todos" (
   "title" varchar NOT NULL,
   "user_id" int NOT NULL,
   "finished" boolean NOT NULL,
-  "deadline" timestamp,
-  "created_at" timestamp NOT NULL DEFAULT 'now ()',
-  "updated_at" timestamp NOT NULL DEFAULT 'now ()'
+  "deadline" timestamptz,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 ALTER TABLE "todos" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
